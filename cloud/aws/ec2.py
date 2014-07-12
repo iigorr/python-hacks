b9678e3d2a2aff1193896c95c3380f614d08f9bf
Igor Lankin <dev@lankin.net>
2014-07-12 17:38:05 +0000

    added wait_for_ready_state method

--- cloud/aws/ec2.py
+++ cloud/aws/ec2.py
@@ -1,4 +1,5 @@
-import os, sys, time, json
+import os, sys, time, json, pprint
+import net
 
 from boto import ec2
 from config import Config
@@ -16,6 +17,8 @@ class Ec2:
                   aws_access_key_id=self.cfg.key_id,
                   aws_secret_access_key=self.cfg.key)  
 
+  def get_connection(self):
+    return self.conn
 
   def create_instances(self, machine_config=None, count=1):
     print 'creating %d instances...' % count
@@ -35,12 +38,12 @@ class Ec2:
     return instance_ids
 
   def wait_for_public_dns(self, instance_ids):
-    print 'waiting for the instance to get their DNS entries...'
+    print 'waiting for instances to get their DNS entries...'
     total_sleep_time = 0 
     sleep_interval = 5
 
     unifinished_ids = list(instance_ids)
-    finished_instances = {}
+    finished_instances = []
 
     for i in range(24):
       instances = self.conn.get_only_instances(unifinished_ids)
@@ -50,7 +53,7 @@ class Ec2:
         if instance.public_dns_name:
           unifinished_ids.remove(instance.id)
           messages.info(instance.id + ': ' + instance.public_dns_name)
-          finished_instances[instance.id] = instance.public_dns_name
+          finished_instances.append({'id': instance.id, 'dns': instance.public_dns_name})
       
       if len(unifinished_ids) == 0:
         break
@@ -65,4 +68,37 @@ class Ec2:
 
     print 'done after ~%d seconds' % total_sleep_time
     messages.ok('Instances: \n%s' % json.dumps(finished_instances, indent=2))
+    return finished_instances
+
+
+  def wait_for_ready_state(self, instance_ids):
+    print 'waiting for instances to become ready...'
+    total_sleep_time = 0 
+    sleep_interval = 10
+
+    unifinished_ids = list(instance_ids)
+    finished_instances = []
+
+    for i in range(24):
+      instances = self.conn.get_only_instances(unifinished_ids)
+
+      for instance in instances:
+        if net.util.check_connection(instance.public_dns_name, 22):
+          unifinished_ids.remove(instance.id)
+          messages.info(instance.id + ' is up and Running')
+          finished_instances.append(instance.id)
+      
+      if len(unifinished_ids) == 0:
+        break
+
+      time.sleep(sleep_interval)
+      total_sleep_time = total_sleep_time + sleep_interval
+    
+    if len(unifinished_ids) > 0: #this means we've timed out
+      messages.warn('Timeout while waiting for machines to boot up. Please check with "aws ec2 describe-instances --instance-ids <id>"' % instance_id)
+      messages.warn('Missing machine ids: %s' + json.dumps(unifinished_ids))
+      return
+
+    print 'done after ~%d seconds' % total_sleep_time
+    messages.ok('Machines up: {0:d} of {1:d}'.format(len(finished_instances), len(instance_ids)))
     return finished_instances
